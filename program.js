f40bf808f2afa4dbd7568b76b85f2e5e15af9dfe
Ian @ epicodus <ian@iancmacdonald.com>
2015-07-28 11:53:24 -0700

    Complete challenges 1 & 2

---
+++ program.js
@@ -0,0 +1,20 @@
+// Challenge 3
+function foo() {
+  var bar;
+  function zip() {
+    var quux;
+  }
+}
+
+// Challenge 2
+// function foo() {
+//   var bar;
+//   function zip() {
+//     var quux;
+//   }
+// }
+
+// Challenge 1
+// function foo() {
+//   var bar;
+// }
